9ab9550bdf96efcbae6539dce47c3aeecca2b436
EricUlrich <EricUlrich@users.noreply.github.com>
2018-12-01 22:45:58 -0700

    Update MainActivity.java
    Added out cycling

--- PitchClicker/app/src/main/java/com/example/ejulrich/pitchclicker/MainActivity.java
+++ PitchClicker/app/src/main/java/com/example/ejulrich/pitchclicker/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends WearableActivity {
     private TextView mTextView;
     Button StrikeButton;
     Button BallButton;
+    Button OutButton;
     TextView StrikeNum;
     TextView BallNum;
     TextView OutNum;
@@ -27,6 +28,7 @@ public class MainActivity extends WearableActivity {
         setContentView(R.layout.activity_main);
         StrikeButton = (Button) findViewById(R.id.Strike_button);
         BallButton = (Button) findViewById(R.id.Ball_button);
+        OutButton = (Button) findViewById(R.id.Out_button);
         StrikeNum = (TextView) findViewById(R.id.Strike_textView);
         BallNum = (TextView) findViewById(R.id.Ball_textView);
         OutNum = (TextView) findViewById(R.id.Out_textView);
@@ -79,7 +81,28 @@ public class MainActivity extends WearableActivity {
                 }
             }
         });
-
+        OutButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mTextView.append("1");
+                OutVal++;
+                if ( OutVal.equals(3) ){
+                    // Will Set out numbers here
+                    StrikeVal = 0;
+                    StrikeNum.setText(StrikeVal.toString());
+                    BallVal = 0;
+                    BallNum.setText((BallVal.toString()));
+                    OutVal = 0;
+                    OutNum.setText((OutVal.toString()));
+                }else{
+                    StrikeVal = 0;
+                    StrikeNum.setText(StrikeVal.toString());
+                    BallVal = 0;
+                    BallNum.setText((BallVal.toString()));
+                    OutNum.setText((OutVal.toString()));
+                }
+            }
+        });
         // Enables Always-on
         setAmbientEnabled();
     }
